fdbfbe98c462e0d788a2da71aa2bd49152aa5b23
Roberto Andrade Salazar <andrade.roberto@uabc.edu.mx>
2022-10-05 14:59:18 -0700

    Agregar archivos de proyecto.

---
+++ Lotus/Lotus/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Lotus.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Lotus.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Lotus/Lotus/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Lotus.Models;
+using Lotus.ViewModels;
+using Lotus.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lotus.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
